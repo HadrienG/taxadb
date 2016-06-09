4fab1706c8b76eac4ea97fbe5492108b0f7b989f
jhayer <juliette.hayer@gmail.com>
2016-06-09 18:51:19 +0200

    Creation of the 3 tables

--- taxa_db.py
+++ taxa_db.py
@@ -7,16 +7,38 @@ from peewee import *
 def main():
     db = SqliteDatabase('test.sqlite')
 
-    class Person(Model):
-        name = CharField()
-        birthday = DateField()
-        is_relative = BooleanField()
+    class Lineage(Model):
+        primary = PrimaryKeyField()
+        level = CharField()
+
+        class Meta:
+            database = db
+
+    class Taxa(Model):
+        primary = PrimaryKeyField()
+        subtaxon_id = ForeignKeyField(rel_model=Model)
+        ncbi_taxid = CharField(null=False)
+        tax_name = CharField()
+        lineage_level = ForeignKeyField(Lineage, to_field='primary')
+
+        class Meta:
+            database = db
+
+    class Sequence(Model):
+        primary = PrimaryKeyField()
+        taxon_id = ForeignKeyField(Taxa, to_field='primary')
+        accession = CharField(null=False)
+        version = IntegerField(null=False)
+        gi = CharField()
+        db_type = CharField(null=False)  # or ForeignKeyField for a table?
 
         class Meta:
             database = db
 
     db.connect()
-    db.create_table(Person)
+    db.create_table(Lineage)
+    db.create_table(Taxa)
+    db.create_table(Sequence)
 
 
 if __name__ == '__main__':
