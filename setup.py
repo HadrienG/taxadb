5290133982de4445180e18da94bd92fbadf53f96
Hadrien Gourlé <gourlehadrien@gmail.com>
2016-07-15 14:13:32 +0200

    wrote setup.py amd moved proj to subfolder

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(name='taxa_db',
+      version='a1',
+
+
+      description='taxa_db',
+      long_descirption=readme,
+
+      url='https://github.com/HadrienG/taxa_db',
+      author='Hadrien Gourlé, Juliette Hayer',
+      author_email='hadrien.gourle@slu.se, juliette.hayer@slu.se',
+
+      license='GPL3',
+      packages=find_packages(exclude=['tests']),
+
+      install_requires=['ftputil', 'peewee', 'pytest'])
--- taxa_db/update_taxonomy.py
+++ taxa_db/update_taxonomy.py
@@ -5,6 +5,8 @@ import gzip
 import ftputil
 import hashlib
 
+NCBI = 'ftp.ncbi.nlm.nih.gov'
+
 # files to download in accession2taxid
 NUCL_EST = 'nucl_est.accession2taxid.gz'
 NUCL_GB = 'nucl_gb.accession2taxid.gz'
@@ -31,22 +33,20 @@ def md5_check(file, block_size=256*128):
 def get_acc2taxid(ACC_DL_LIST):
     for file in ACC_DL_LIST:
         print('Started Downloading %s' % (file))
-        # TODO: have a try except around the connection
-        ncbi = ftputil.FTPHost('ftp.ncbi.nlm.nih.gov', 'anonymous', 'password')
-        ncbi.chdir('pub/taxonomy/accession2taxid/')
-        ncbi.download_if_newer(file, file)
-        ncbi.download_if_newer(file + '.md5', file + '.md5')
-        md5_check(file)
+        with ftputil.FTPHost(NCBI, 'anonymous', 'password') as ncbi:
+            ncbi.chdir('pub/taxonomy/accession2taxid/')
+            ncbi.download_if_newer(file, file)
+            ncbi.download_if_newer(file + '.md5', file + '.md5')
+            md5_check(file)
 
 
 def get_taxdump(TAXDUMP):
     print('Started Downloading %s' % (TAXDUMP))
-    # TODO: have a try except around the connection
-    ncbi = ftputil.FTPHost('ftp.ncbi.nlm.nih.gov', 'anonymous', 'password')
-    ncbi.chdir('pub/taxonomy/')
-    ncbi.download_if_newer(TAXDUMP, TAXDUMP)
-    ncbi.download_if_newer(TAXDUMP + '.md5', TAXDUMP + '.md5')
-    md5_check(TAXDUMP)
+    with ftputil.FTPHost(NCBI, 'anonymous', 'password') as ncbi:
+        ncbi.chdir('pub/taxonomy/')
+        ncbi.download_if_newer(TAXDUMP, TAXDUMP)
+        ncbi.download_if_newer(TAXDUMP + '.md5', TAXDUMP + '.md5')
+        md5_check(TAXDUMP)
 
 
 def main():
