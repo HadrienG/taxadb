c90f878df57970c11c8703e53216003cd8029df9
Hadrien Gourlé <gourlehadrien@gmail.com>
2016-07-18 18:26:37 +0200

    added test for db_create

--- tests/db_tests.py
+++ tests/db_tests.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import subprocess
+from taxa_db import create_db
 
-def test():
-    pass
+
+def db_install_test():
+    create_db()
+    proc = subprocess.Popen(
+        ['sqlite3', 'test.sqlite', '.tables'],
+        stdout=subprocess.PIPE)
+    db_call = proc.stdout.read()
+    assert db_call == b'lineage   sequence  taxa    \n'
