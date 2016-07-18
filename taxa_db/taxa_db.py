48e44c61ab42139748b937fef1708cedf04545d0
jhayer <juliette.hayer@gmail.com>
2016-07-18 19:19:59 +0200

    took Class tables out of function

--- taxa_db/taxa_db.py
+++ taxa_db/taxa_db.py
@@ -4,42 +4,33 @@
 from peewee import *
 
 
+class Taxa(Model):
+    primary = PrimaryKeyField()
+    ncbi_taxid = IntegerField(null=False)
+    parent_taxid = ForeignKeyField(rel_model=Model)
+    tax_name = CharField()
+    lineage_level = CharField()
+
+
+class Sequence(Model):
+    primary = PrimaryKeyField()
+    taxon_id = ForeignKeyField(Taxa, to_field='primary')
+    accession = CharField(null=False)
+    version = IntegerField(null=False)
+    gi = CharField()
+    db_type = CharField(null=False)  # or ForeignKeyField for a table?
+
+
 def create_db():
     db = SqliteDatabase('test.sqlite')
-
-    class Lineage(Model):
-        primary = PrimaryKeyField()
-        level = CharField()
-
-        class Meta:
-            database = db
-
-    class Taxa(Model):
-        primary = PrimaryKeyField()
-        subtaxon_id = ForeignKeyField(rel_model=Model)
-        ncbi_taxid = CharField(null=False)
-        tax_name = CharField()
-        lineage_level = ForeignKeyField(Lineage, to_field='primary')
-
-        class Meta:
-            database = db
-
-    class Sequence(Model):
-        primary = PrimaryKeyField()
-        taxon_id = ForeignKeyField(Taxa, to_field='primary')
-        accession = CharField(null=False)
-        version = IntegerField(null=False)
-        gi = CharField()
-        db_type = CharField(null=False)  # or ForeignKeyField for a table?
-
-        class Meta:
-            database = db
-
     db.connect()
     db.create_table(Lineage)
     db.create_table(Taxa)
     db.create_table(Sequence)
 
+
+def main():
+    create_db()
 
 if __name__ == '__main__':
     main()
