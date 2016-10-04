a198062f3f875536c8886a030d29b654cf6f3f08
Hadrien Gourlé <gourlehadrien@gmail.com>
2016-10-04 16:15:23 -0300

    fixed peewee version to 2.8.1

--- taxa_db/taxadb_create.py
+++ taxa_db/taxadb_create.py
@@ -1,38 +1,64 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import taxa_db
-from peewee import *
+# from peewee import *
+import peewee as pw
+
+
+db = pw.SqliteDatabase('test.sqlite')
+
+
+class BaseModel(pw.Model):
+    class Meta:
+        database = db
+
+
+class Taxa(BaseModel):
+    primary = pw.PrimaryKeyField()
+    ncbi_taxid = pw.IntegerField(null=False)
+    parent_taxid = pw.ForeignKeyField(rel_model=pw.Model)
+    tax_name = pw.CharField()
+    lineage_level = pw.CharField()
+
+
+class Sequence(BaseModel):
+    primary = pw.PrimaryKeyField()
+    taxon_id = pw.ForeignKeyField(Taxa, to_field='primary')
+    accession = pw.CharField(null=False)
+    version = pw.IntegerField(null=False)
+    gi = pw.CharField()
+    db_type = pw.CharField(null=False)  # or ForeignKeyField for a table?
+
+
+def create_db(db):
+    db.connect()
+    db.create_table(Taxa)
+    db.create_table(Sequence)
 
 
 def parse_node(node_file):
-
+    node_data = list()
     with open(node_file, 'r') as f:
         for line in f:
             line_lst = line.rstrip('\t').split('|')
-            print(line_lst)
-            taxid = line_lst[0]
-            parent_id = line_lst[1]
-            rank = line_lst[2]
 
-            cur_tax = Taxa.create(
-                ncbi_taxid=taxid,
-                parent_taxid=parent_id,
-                tax_name='',
-                lineage_level=rank
-            )
-            break
-    # uncle_bob = Person(
-        # name='Bob',
-        # birthday=date(1960, 1, 15),
-        # is_relative=True)
-    # uncle_bob.save() # bob is now stored in the database
+            data_dict = {
+                'ncbi_taxid': line_lst[0],
+                'parent_taxid': line_lst[1],
+                'tax_name': '',
+                'lineage_level': line_lst[2]}
+            node_data.append(data_dict)
+
+        with db.atomic():
+            for i in range(0, len(node_data), 500):
+                Taxa.insert_many(node_data[i:i+500]).execute()
+            print('%s rows added' % (len(node_data)))
+        db.close()
 
 
 def main():
-    db = SqliteDatabase('test.sqlite')
-    db.connect()
-    parse_node("data/nodes.dmp")
+    create_db(db)
+    parse_node("nodes.dmp")
 
 
 if __name__ == '__main__':
