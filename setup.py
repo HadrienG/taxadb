adde46c0e136de5523c4658a1a63edd7cfd00e7f
Hadrien Gourlé <gourlehadrien@gmail.com>
2016-11-14 13:09:18 +0100

    cleaned up old files

--- setup.py
+++ setup.py
@@ -6,18 +6,24 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     readme = f.read()
 
-setup(name='taxa_db',
-      version='a1',
+setup(
+    name='taxadb',
+    version='a2',
 
 
-      description='taxa_db',
-      long_descirption=readme,
+    description='taxadb',
+    long_descirption=readme,
 
-      url='https://github.com/HadrienG/taxa_db',
-      author='Hadrien Gourlé, Juliette Hayer',
-      author_email='hadrien.gourle@slu.se, juliette.hayer@slu.se',
+    url='https://github.com/HadrienG/taxa_db',
+    author='Hadrien Gourlé, Juliette Hayer',
+    author_email='hadrien.gourle@slu.se, juliette.hayer@slu.se',
 
-      license='GPL3',
-      packages=find_packages(exclude=['tests']),
+    license='GPL3',
+    packages=find_packages(exclude=['tests']),
 
-      install_requires=['ftputil', 'peewee', 'nose'])
+    install_requires=['ftputil', 'peewee', 'nose'],
+
+    entry_points={
+        'console_scripts': ['taxadb = taxadb.app:main']
+    }
+      )
--- taxadb/__main__.py
+++ taxadb/__main__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from .taxa_db import *
+from taxadb.app import main
+main()
