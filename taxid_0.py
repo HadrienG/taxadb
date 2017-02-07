e67283aa1bf73705dffba7bde1e4a8724ebff582
horkko <horkko@gmail.com>
2017-02-07 17:56:44 +0100

    Changed to sqlite use

--- taxid_0.py
+++ taxid_0.py
@@ -3,6 +3,7 @@
 import sys
 import os
 from taxadb import accession
+from taxadb import taxid
 from taxadb.schema import *
 
 TAXADB_DIR = '/data/sqlite3'
@@ -12,7 +13,7 @@ sqlite = os.path.join(TAXADB_DIR, TAXADB_DBNAME + '_full.sqlite')
 postgres = TAXADB_DBNAME
 accessions = ['AAA00056', 'AAA00059', 'AAA00053']
 print("Query Gss table ...")
-taxids = accession.taxid(accessions, postgres, Prot, user='tuco', password='tucobell')
+taxids = accession.taxid(accessions, sqlite, Prot, user='tuco', password='tucobell')
 for tax in taxids:
     print(tax)
 print("Query Prot table ...")
