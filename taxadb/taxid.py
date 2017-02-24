b3aa163dbde049c296d271d7bcc5813e8be805b3
horkko <horkko@gmail.com>
2017-02-24 23:42:25 +0100

    Grouped methods into one unique class Taxid

--- taxadb/taxid.py
+++ taxadb/taxid.py
@@ -1,89 +1,86 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from taxadb.schema import *
+from taxadb.schema import Taxa, DatabaseFactory, db
 
 
-def sci_name(taxid, db_name, **kwargs):
-    """given a taxid, return its associated scientific name
+class Taxid(object):
+    """Main class for querying taxid"""
 
-    You can access data from several database type (sqlite3/mysql/postgresql)
-    To do so, call the method as follow:
-    sqlite3:    sci_name = taxid.sci_name(3309, '/path/to/db.sqlite')
-    mysql:      sci_name = taxid.sci_name(3309, 'dbname', dbtype='mysql', user='user', password='secret')
-    postgresql: sci_name = taxid.sci_name(3309, 'dbname', dbtype='postgres', user='user', password='secret')
+    def __init__(self, dbtype=None, dbname=None, **kwargs):
+        self.db = db
+        self.dbname = dbname
+        self.database = DatabaseFactory(dbname=dbname, dbtype=dbtype, **kwargs).get_database()
+        self.db.initialize(self.database)
+        self.db.connect()
 
-    Arguments:
-    taxid -- a taxid (int)
-    db_name -- the path to the database to query
-    kwargs -- Extra options for non sqlite database type (e.g.: dbtype/username/password)
-    """
-    database = DatabaseFactory(dbname=db_name, **kwargs).get_database()
-    db.initialize(database)
-    db.connect()
-    name = Taxa.get(Taxa.ncbi_taxid == taxid).tax_name
-    db.close()
-    return name
+    def __del__(self):
+        self.db.close()
 
+    def sci_name(self, taxid):
+        """given a taxid, return its associated scientific name
 
-def lineage_id(taxid, db_name, **kwargs):
-    """given a taxid, return its associated lineage (in the form of a list
-    of taxids, each parents of each others)
+        You can access data from several database type (sqlite3/mysql/postgresql)
+        Arguments:
+        taxid -- a taxid (int)
+        Returns:
+        name -- scientific name (str) or None if taxid not found
+        """
+        try:
+            name = Taxa.get(Taxa.ncbi_taxid == taxid).tax_name
+            return name
+        except Taxa.DoesNotExist as err:
+            return None
 
-    You can access data from several database type (sqlite3/mysql/postgresql)
-    To do so, call the method as follow:
-    sqlite3:    lineage_id = taxid.lineage_name(3309, '/path/to/db.sqlite')
-    mysql:      lineage_id = taxid.lineage_name(3309, 'dbname', dbtype='mysql', user='user', password='secret')
-    postgresql: lineage_id = taxid.lineage_name(3309, 'dbname', dbtype='postgres', user='user', password='secret')
+    def lineage_id(self, taxid, reverse=False):
+        """given a taxid, return its associated lineage (in the form of a list
+        of taxids, each parents of each others)
 
-    Arguments:
-    taxid -- a taxid (int)
-    db_name -- the path to the database to query
-    kwargs -- Extra options for non sqlite database type (e.g.: dbtype/username/password)
-    """
-    database = DatabaseFactory(dbname=db_name, **kwargs).get_database()
-    db.initialize(database)
-    db.connect()
-    lineage_list = []
-    current_lineage = Taxa.get(Taxa.ncbi_taxid == taxid).tax_name
-    current_lineage_id = Taxa.get(Taxa.ncbi_taxid == taxid).ncbi_taxid
-    parent = Taxa.get(Taxa.ncbi_taxid == taxid).parent_taxid
-    while current_lineage != 'root':
-        lineage_list.append(current_lineage_id)
-        new_query = Taxa.get(Taxa.ncbi_taxid == parent)
+        Arguments:
+        taxid -- a taxid (int)
+        reverse -- Inverted lineage, from top to bottom taxonomy hierarchy
+        Returns:
+        lineage_list -- associated lineage id with taxid (list) or None if taxid not found
+        """
+        try:
+            lineage_list = []
+            current_lineage = Taxa.get(Taxa.ncbi_taxid == taxid).tax_name
+            current_lineage_id = Taxa.get(Taxa.ncbi_taxid == taxid).ncbi_taxid
+            parent = Taxa.get(Taxa.ncbi_taxid == taxid).parent_taxid
+            while current_lineage != 'root':
+                lineage_list.append(current_lineage_id)
+                new_query = Taxa.get(Taxa.ncbi_taxid == parent)
 
-        current_lineage = new_query.tax_name
-        current_lineage_id = new_query.ncbi_taxid
-        parent = new_query.parent_taxid
-    return lineage_list
-    db.close()
+                current_lineage = new_query.tax_name
+                current_lineage_id = new_query.ncbi_taxid
+                parent = new_query.parent_taxid
+            if reverse is True:
+                lineage_list.reverse()
+            return lineage_list
+        except Taxa.DoesNotExist as err:
+            return None
 
+    def lineage_name(slef, taxid, reverse=False):
+        """given a taxid, return its associated lineage
 
-def lineage_name(taxid, db_name, **kwargs):
-    """given a taxid, return its associated lineage
+        Arguments:
+        taxid -- a taxid (int)
+        reverse -- Inverted lineage, from top to bottom taxonomy hierarchy
+        Returns:
+        lineage_name -- associated lineage name with taxid (list) or None if taxid not found
+        """
+        try:
+            lineage_list = []
+            current_lineage = Taxa.get(Taxa.ncbi_taxid == taxid).tax_name
+            parent = Taxa.get(Taxa.ncbi_taxid == taxid).parent_taxid
+            while current_lineage != 'root':
+                lineage_list.append(current_lineage)
+                new_query = Taxa.get(Taxa.ncbi_taxid == parent)
 
-    You can access data from several database type (sqlite3/mysql/postgresql)
-    To do so, call the method as follow:
-    sqlite3:    lineage = taxid.lineage_name(3309, '/path/to/db.sqlite')
-    mysql:      lineage = taxid.lineage_name(3309, 'dbname', dbtype='mysql', user='user', password='secret')
-    postgresql: lineage = taxid.lineage_name(3309, 'dbname', dbtype='postgres', user='user', password='secret')
-
-    Arguments:
-    taxid -- a taxid (int)
-    db_name -- the path to the database to query
-    kwargs -- Extra options for non sqlite database type (e.g.: dbtype/user/password)
-    """
-    database = DatabaseFactory(dbname=db_name, **kwargs).get_database()
-    db.initialize(database)
-    db.connect()
-    lineage_list = []
-    current_lineage = Taxa.get(Taxa.ncbi_taxid == taxid).tax_name
-    parent = Taxa.get(Taxa.ncbi_taxid == taxid).parent_taxid
-    while current_lineage != 'root':
-        lineage_list.append(current_lineage)
-        new_query = Taxa.get(Taxa.ncbi_taxid == parent)
-
-        current_lineage = new_query.tax_name
-        parent = new_query.parent_taxid
-    return lineage_list
-    db.close()
+                current_lineage = new_query.tax_name
+                parent = new_query.parent_taxid
+            if reverse is True:
+                lineage_list.reverse()
+            return lineage_list
+        except Taxa.DoesNotExist as err:
+            return None
