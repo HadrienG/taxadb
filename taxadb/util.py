ff150dbefe495972e6cf2f8abb6ec1d7e400eae6
Hadrien Gourlé <gourlehadrien@gmail.com>
2018-03-12 11:35:34 +0100

    better md5 reporting

--- taxadb/util.py
+++ taxadb/util.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import hashlib
 import sys
+import hashlib
 import logging
 
 
 def md5_check(file, block_size=256*128):
-    """Check the md5 of large files
+    """Check the md5 of files large or small
 
     Args:
-        file (:obj:`str`): input file
-        block_size (:obj:`int`): block_size for the file chunks.
-            Default = 256*128
+        file (str): input file
+        block_size (int): block_size for the file chunks. Default = 256*128
     """
     logger = logging.getLogger(__name__)
 
@@ -22,8 +21,14 @@ def md5_check(file, block_size=256*128):
     with open(file, 'rb') as f:
         for chunk in iter(lambda: f.read(block_size), b''):
             file_md5.update(chunk)
-    assert(file_md5.hexdigest() == md5)
-    logger.info('Checking md5 of %s: OK' % file)
+    try:
+        assert(file_md5.hexdigest() == md5)
+    except AssertionError as e:
+        logger.error('Checking md5 of %s: NOT OK' % file)
+        logger.error('Incomplete download of %s. Aborting' % file)
+        sys.exit(1)
+    else:
+        logger.info('Checking md5 of %s: OK' % file)
 
 
 def fatal(msg):
